7f7fd903661ed401e47ec938bc13280b8fefc2fd
Mitra Raman <mitraraman@gmail.com>
2013-10-25 13:30:36 -0400

    default view

---
+++ Logger/Logger/LJDefaultViewController.h
@@ -0,0 +1,17 @@
+//
+//  LJDefaultViewController.h
+//  Logger
+//
+//  Created by Mitra Raman on 10/25/13.
+//  Copyright (c) 2013 yeung.steph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJDefaultViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *myHours;
+@property (strong, nonatomic) IBOutlet UILabel *partnerHours;
+- (IBAction)selectPartner:(id)sender;
+- (IBAction)logHours:(id)sender;
+
+@end
---
+++ Logger/Logger/LJDefaultViewController.m
@@ -0,0 +1,45 @@
+//
+//  LJDefaultViewController.m
+//  Logger
+//
+//  Created by Mitra Raman on 10/25/13.
+//  Copyright (c) 2013 yeung.steph. All rights reserved.
+//
+
+#import "LJDefaultViewController.h"
+
+@interface LJDefaultViewController ()
+
+@end
+
+@implementation LJDefaultViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)selectPartner:(id)sender {
+}
+
+- (IBAction)logHours:(id)sender {
+}
+- (IBAction)selectPartner:(id)sender {
+}
+@end
