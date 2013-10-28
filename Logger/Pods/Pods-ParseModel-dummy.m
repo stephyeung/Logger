4a3eeaef990d063e7a3667661eadd524b02eb7ed
Stephanie <stephaniexyeung@gmail.com>
2013-10-27 20:34:41 -0400

    Pods

---
+++ Logger/Pods/Pods-ParseModel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParseModel : NSObject
+@end
+@implementation PodsDummy_Pods_ParseModel
+@end
---
+++ Logger/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DSLCalendarView
+#define COCOAPODS_POD_AVAILABLE_DSLCalendarView
+#define COCOAPODS_VERSION_MAJOR_DSLCalendarView 0
+#define COCOAPODS_VERSION_MINOR_DSLCalendarView 0
+#define COCOAPODS_VERSION_PATCH_DSLCalendarView 1
+
+// Facebook-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Facebook_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Facebook_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_Facebook_iOS_SDK 9
+#define COCOAPODS_VERSION_PATCH_Facebook_iOS_SDK 0
+
+// Parse-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Parse_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Parse_iOS_SDK 1
+#define COCOAPODS_VERSION_MINOR_Parse_iOS_SDK 2
+#define COCOAPODS_VERSION_PATCH_Parse_iOS_SDK 15
+
